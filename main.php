ca90ebc1c0558ca85441f4a87808dc0dd471f02f
Jana Deutschländer <deutschlaender@cosmocode.de>
2016-11-03 17:21:39 +0100

    magic matcher / direct nav / reset styles

--- main.php
+++ main.php
@@ -16,6 +16,10 @@ header('X-UA-Compatible: IE=edge,chrome=1');
 $showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && !empty($_SERVER['REMOTE_USER']) );
 $showSidebar = page_findnearest($conf['sidebar']) && ($ACT=='show');
 
+
+/* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + */
+/* conditional comments for IE8 / IE9 browser detection if needed */
+/* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + */
 ?>
 <!--[if lt IE 9]> <html class="no-js lt-ie10 lt-ie9" lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>"> <![endif]-->
 <!--[if IE 9]> <html class="no-js lt-ie10 ie-9" lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>"> <![endif]-->
@@ -36,10 +40,13 @@ $showSidebar = page_findnearest($conf['sidebar']) && ($ACT=='show');
 
 
 /* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + */
-/* IE8 support HTML5 / media queries */
+/* conditional comments for HTML5 / media queries support in IE8 */
 /* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + */
 ?>
-<!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 <?php
 
@@ -59,26 +66,10 @@ $showSidebar = page_findnearest($conf['sidebar']) && ($ACT=='show');
 /* favicons */
 /* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + */
 ?>
-<?php echo tpl_favicon(array('favicon')) ?> <?php /* DokuWiki: favicon.ico  */ ?>
-
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-57x57.png', 'images/fav/apple-touch-icon-57x57.png')); ?>">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-60x60.png', 'images/fav/apple-touch-icon-60x60.png')); ?>">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-72x72.png', 'images/fav/apple-touch-icon-72x72.png')); ?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-76x76.png', 'images/fav/apple-touch-icon-76x76.png')); ?>">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-114x114.png', 'images/fav/apple-touch-icon-114x114.png')); ?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-120x120.png', 'images/fav/apple-touch-icon-120x120.png')); ?>">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-144x144.png', 'images/fav/apple-touch-icon-144x144.png')); ?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-152x152.png', 'images/fav/apple-touch-icon-152x152.png')); ?>">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-180x180.png', 'images/fav/apple-touch-icon-180x180.png')); ?>">
-
-<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:favicon-32x32.png', 'images/fav/favicon-32x32.png')); ?>" sizes="32x32">
-<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:favicon-96x96.png', 'images/fav/favicon-96x96.png')); ?>" sizes="96x96">
-<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:android-chrome-192x192.png', 'images/fav/android-chrome-192x192.png')); ?>" sizes="192x192">
-
-<meta name="msapplication-square70x70logo" content="<?php echo tpl_getMediaFile(array(':wiki:smalltile.png', 'images/fav/smalltile.png')); ?>" />
-<meta name="msapplication-square150x150logo" content="<?php echo tpl_getMediaFile(array(':wiki:mediumtile.png', 'images/fav/mediumtile.png')); ?>" />
-<meta name="msapplication-wide310x150logo" content="<?php echo tpl_getMediaFile(array(':wiki:widetile.png', 'images/fav/widetile.png')); ?>" />
-<meta name="msapplication-square310x310logo" content="<?php echo tpl_getMediaFile(array(':wiki:largetile.png', 'images/fav/largetile.png')); ?>" />
+<?php
+    echo tpl_favicon(array('favicon')); /* DokuWiki: favicon.ico  */
+    include('tpl/favicon_tiles.php');
+?>
 <?php
 
 
---
+++ tpl/favicon_tiles.php
@@ -0,0 +1,23 @@
+<?php
+    if (!defined('DOKU_INC')) die();
+?>
+
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-57x57.png', 'images/fav/apple-touch-icon-57x57.png')); ?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-60x60.png', 'images/fav/apple-touch-icon-60x60.png')); ?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-72x72.png', 'images/fav/apple-touch-icon-72x72.png')); ?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-76x76.png', 'images/fav/apple-touch-icon-76x76.png')); ?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-114x114.png', 'images/fav/apple-touch-icon-114x114.png')); ?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-120x120.png', 'images/fav/apple-touch-icon-120x120.png')); ?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-144x144.png', 'images/fav/apple-touch-icon-144x144.png')); ?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-152x152.png', 'images/fav/apple-touch-icon-152x152.png')); ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo tpl_getMediaFile(array(':wiki:apple-touch-icon-180x180.png', 'images/fav/apple-touch-icon-180x180.png')); ?>">
+
+<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:favicon-32x32.png', 'images/fav/favicon-32x32.png')); ?>" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:favicon-96x96.png', 'images/fav/favicon-96x96.png')); ?>" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo tpl_getMediaFile(array(':wiki:android-chrome-192x192.png', 'images/fav/android-chrome-192x192.png')); ?>" sizes="192x192">
+
+<meta name="msapplication-square70x70logo" content="<?php echo tpl_getMediaFile(array(':wiki:smalltile.png', 'images/fav/smalltile.png')); ?>" />
+<meta name="msapplication-square150x150logo" content="<?php echo tpl_getMediaFile(array(':wiki:mediumtile.png', 'images/fav/mediumtile.png')); ?>" />
+<meta name="msapplication-wide310x150logo" content="<?php echo tpl_getMediaFile(array(':wiki:widetile.png', 'images/fav/widetile.png')); ?>" />
+<meta name="msapplication-square310x310logo" content="<?php echo tpl_getMediaFile(array(':wiki:largetile.png', 'images/fav/largetile.png')); ?>" />
+
