da7e18f85225d1e61d7b08348ad958fde47c2bc7
Jana Deutschländer <deutschlaender@cosmocode.de>
2016-12-13 17:13:23 +0100

    nav-tools menu icons

--- tpl/nav-usertools.php
+++ tpl/nav-usertools.php
@@ -6,16 +6,14 @@
                         <nav id="dokuwiki__usertools" class="nav-usertools">
                             <h6 class="sr-only" role="heading" aria-level="2"><?php echo $lang['user_tools']; ?></h6>
                             <ul>
+                                <li class="log"><?php tpl_actionlink('login'); ?></li>
                                 <?php
                                 if (!empty($_SERVER['REMOTE_USER'])) {
                                     echo '<li class="user"><span class="sr-only">'.$lang['loggedinas'].' </span>'.userlink().'</li>';
                                 }?>
-                                <li class="log"><?php tpl_actionlink('login'); ?></li>
 
                                 <?php tpl_toolsevent('usertools', array(
                                     'admin'     => tpl_action('admin', 1, 'li', 1),
-                                    'userpage'  => _tpl_action('userpage', 1, 'li', 1),
-                                    'profile'   => tpl_action('profile', 1, 'li', 1),
                                     'register'  => tpl_action('register', 1, 'li', 1),
                                 )); ?>
 
