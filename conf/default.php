83efa0de010dbd4219c46aa24e98e0c7f68bf1c8
Jana Deutschländer <deutschlaender@cosmocode.de>
2017-01-05 18:23:56 +0100

    mobile view part 1

--- conf/default.php
+++ conf/default.php
@@ -5,6 +5,8 @@
  */
 
 $conf['logo']             = 'logo.png';
+$conf['mobile_logo']      = 'images/fav/favicon-32x32.png';
+
 $conf['logo_spacer']      = 'logo_spacer.png';
 
 $conf['discussionPage']   = 'discussion:@ID@';
--- tpl/main-sidebar-logo.php
+++ tpl/main-sidebar-logo.php
@@ -4,8 +4,11 @@
 
     /* homepage logo should not link to itself (BITV accessibility requirement) */
     if (strcmp(wl(), $_SERVER['REQUEST_URI']) === 0 ){
-        echo '<img src="'.ml(tpl_getConf('logo')).'" alt="'.tpl_getLang('adjunct_start_logo_text').$conf['title'].'" />';
+        echo '<img class="mobile-hide" src="'.ml(tpl_getConf('logo')).'" alt="'.tpl_getLang('adjunct_start_logo_text').$conf['title'].'" />';
+        echo '<img class="mobile-only" src="'.tpl_getMediaFile(array(':wiki:favicon-32x32.png', 'images/fav/favicon-32x32.png')).'" alt="'.tpl_getLang('adjunct_start_logo_text').$conf['title'].'" />';
+
     } else{
-        tpl_link( wl(),'<img src="'.ml(tpl_getConf('logo')).'" alt="'.$conf['title'].tpl_getLang('adjunct_linked_logo_text').'" />','accesskey="h" title="[H]"' );
+        tpl_link( wl(),'<img class="mobile-hide" src="'.ml(tpl_getConf('logo')).'" alt="'.$conf['title'].tpl_getLang('adjunct_linked_logo_text').'" />','accesskey="h" title="[H]"' );
+        tpl_link( wl(),'<img class="mobile-only" src="'.tpl_getMediaFile(array(':wiki:favicon-32x32.png', 'images/fav/favicon-32x32.png')).'" alt="'.$conf['title'].tpl_getLang('adjunct_linked_logo_text').'" />','accesskey="h" title="[H]"' );
     }
     echo "<hr class=\"structure\" /></div>";
