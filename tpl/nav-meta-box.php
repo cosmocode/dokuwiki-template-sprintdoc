d62df8bdbf5da94fe902027cc76c3cff132840d1
Jana Deutschländer <deutschlaender@cosmocode.de>
2017-01-09 16:31:32 +0100

    metabox part 2

--- tpl/nav-meta-box.php
+++ tpl/nav-meta-box.php
@@ -1,17 +1,23 @@
-<ul class="nav-tabs">
-    <li><a data-toggle="tab" href="#tab1" aria-expanded="false"><span class="prefix">Sitemap</span><span class="num"></span></a></li>
-    <li><a data-toggle="tab" href="#tab2" aria-expanded="false"><span class="prefix">Jira</span><span class="num"></span></a></li>
-    <li class="active"><a data-toggle="tab" href="#tab3" aria-expanded="true"><span class="prefix">Tags</span><span class="num"></span></a></li>
-</ul>
+<div class="tab-container">
+    <ul class="meta-tabs">
+        <!--<li class="active"><a data-toggle="tab" href="#tab1" aria-expanded="true"><span class="prefix">Sitemap</span><span class="num"></span></a></li>-->
+        <li><a data-toggle="tab" href="#tab1" aria-expanded="false"><span class="prefix">Sitemap</span><span class="num"></span></a></li>
+        <li><a data-toggle="tab" href="#tab2" aria-expanded="false"><span class="prefix">Tags</span><span class="num"></span></a></li>
+        <li><a data-toggle="tab" href="#tab3" aria-expanded="false"><span class="prefix">Jira</span><span class="num"></span></a></li>
 
-<div class="tab-content">
-    <div id="tab1" class="tab-pane fade in active">
+    </ul>
 
-    </div>
-    <div id="tab2" class="tab-pane fade">
+    <div class="meta-content">
+        <div class="box-content">
+            <div id="tab1" class="tab-pane"> <!-- .active -->
+                <?php  tpl_toc(); ?>
+            </div>
+            <div id="tab2" class="tab-pane">
 
-    </div>
-    <div id="tab3" class="tab-pane fade">
+            </div>
+            <div id="tab3" class="tab-pane">
 
+            </div>
+        </div>
     </div>
 </div>
