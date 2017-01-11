659ce8325fa0170b1e088cc33c5a630daa146073
Jana Deutschländer <deutschlaender@cosmocode.de>
2017-01-11 15:51:10 +0100

    merge mit spis vorbereiten

--- tpl/main-sidebar-search.php
+++ tpl/main-sidebar-search.php
@@ -1,6 +1,6 @@
 <?php
     if (!defined('DOKU_INC')) die();
 
-    echo '<h6 class="sr-only" role="heading" aria-level="2">'.tpl_getLang('head_quick_search').'</h6>';
-
-    tpl_searchform();
+    echo '<h6 class="sr-only" role="heading" aria-level="2">'.tpl_getLang('head_quick_search').'</h6>'; ?>
+    <p><a href="#qsearch__out"><span class="prefix">Zur Suche springen</span></a></p>
+<?php tpl_searchform(); ?>
