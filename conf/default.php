55a903a9da9bfb399fb467d0996f37a12681c52e
Andreas Gohr <gohr@cosmocode.de>
2017-02-09 17:37:38 +0100

    changed default logo location (and line endings)

--- conf/default.php
+++ conf/default.php
@@ -4,7 +4,7 @@
  *
  */
 
-$conf['logo']             = 'logo.png';
+$conf['logo']             = 'wiki:logo.png';
 $conf['mobile_logo']      = 'images/fav/favicon-32x32.png';
 
 $conf['logo_spacer']      = 'logo_spacer.png';
