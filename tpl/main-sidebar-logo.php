72d714f8ea6dceeb06572d1fcb81dcad2b0fb82a
Silke Pisulla <pisulla@cosmocode.de>
2017-03-03 10:52:53 +0100

    SPR-843: Fontello Icons umstellen auf Material Design Icons

--- tpl/main-sidebar-logo.php
+++ tpl/main-sidebar-logo.php
@@ -3,7 +3,7 @@ if(!defined('DOKU_INC')) die();
 
 echo '<div class="menu-togglelink mobile-only">';
 echo '<a href="#">';
-echo inlineSVG(__DIR__ . '/../img/menu.svg');
+//echo inlineSVG(__DIR__ . '/../img/menu.svg');
 echo '<span class="sr-out">'.tpl_getLang('a11y_sidebartoggle').'</span>';
 echo '</a>';
 echo '</div>';
