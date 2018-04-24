9038109bd65fca045efce22c6c6b701231ebed35
Michael Große <grosse@cosmocode.de>
2018-04-24 15:31:08 +0200

    fix: remove non-existing include, add placeholder comment

--- tpl/nav-status.php
+++ tpl/nav-status.php
@@ -2,5 +2,5 @@
     if (!defined('DOKU_INC')) die();
 ?>
 <div class="nav-status">
-    <?php tpl_include_page($conf['statusbar'], 1, 1) /* includes the nearest statusbar page */ ?>
+    <!-- FIXME: structstatus integration -->
 </div>
