902592407d19f2d8517fddccd49033c9b8ee3433
Roberto Bellingeri <bellingeri@netguru.it>
2019-03-18 11:28:01 +0100

    Create lang.php

---
+++ lang/it/lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * File di lingua Italiana per il template
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+
+$lang['direct_prefix']              = 'Vai a';
+$lang['direct_content_main']        = 'Contenuto Principale';
+$lang['direct_quick_search']        = 'Ricerca';
+$lang['direct_content_toggle']      = 'Cambia Larghezza del Contenuto';
+
+$lang['adjunct_linked_logo_text']   = ' - link alla homepage';
+$lang['a11y_sidebartoggle']         = 'Mostra/Nascondi Navigazione';
+
+$lang['nav-area-head']              = 'Menu e Ricerca rapida';
+$lang['head_magic_matcher']         = 'MagicMatcher';
+$lang['head_quick_search']          = 'Ricerca rapida';
+$lang['head_menu_status']           = 'Stato del sito';
+$lang['head_breadcrumb_youarehere'] = 'Indicatore di posizione';
+$lang['head_menu_trace']            = 'Ultime Pagine Visitate';
+$lang['head_meta_box']              = 'meta dati per questa pagina';
+$lang['head_menu_starred']          = 'Pagine Preferite';
+$lang['head_menu_tool-select']      = 'Strumenti';
+
+$lang['jump_to_quicksearch']        = 'Vai alla ricerca rapida';
+
+/** it will be shown from plugin lang.php:
+$lang['meta_box_jira_tickets_none'] = 'Ticket jira associati trovati: nessuno';
+$lang['meta_box_tags_none']         = 'Tag trovati: nessuno';
+$lang['js']['meta_box_toc_none']    = 'Nessun Sommario disponibile';
+$lang['prefix_tasks_user']          = 'I tuoi task aperti: ';
+*/
+
+$lang['tab_tags']                   = 'Tag';
+$lang['tab_issues']                 = 'Problemi';
+
+$lang['quality_trigger']            = 'Mostra/Nascondi l\'analisi della pagina';
+
+$lang['prefix_tasks_page']          = 'aprire le attività per questa pagina: ';
+
+$lang['tasks_page_none']            = 'nessuna attività aperta in questa pagina';
+$lang['tasks_page_intime']          = 'attività aperte su questa pagina: %1$d';
+$lang['tasks_page_late']            = 'attività aperte su questa pagina: %1$d, troppo tardi: %2$d';
+
+/** is not used
+$lang['discussion']                 = 'Discussione';
+$lang['back_to_article']            = 'Torna all'articolo';
+$lang['userpage']                   = 'Pagina utente';
+*/
+
+$lang['meta_data']                  = 'Meta dati dell\'immagine';
+$lang['osm_zoom_link_text']         = 'visualizza una mappa più grande';
+$lang['osm_zoom_link_title']        = 'link esterno - si apre in una nuova finestra';
+
+$lang['image_detail']               = 'Dettagli dell\'immagine';
+
+$lang['mode_edit']                  = 'Modifica pagina';
+
+
+$lang['nav_sidebar']                = 'Navigazione';
+$lang['nav_sitemap']                = 'Indice';
+
+/**
+ * preferences colors
+ */
+
+$lang['__site_width__']             = 'Contenuto - Larghezza (in %)';
+
+$lang['__background_site__']        = 'Sito Web - Colore di sfondo';
+$lang['__text_webframe__']          = 'Claim, Titolo, Pi&egrave; di pagina (non collegato), Navigazione (non collegata) - Colore del carattere';
+
+$lang['__nav_menu_color__']         = 'Navigazione (collegamento),  Pi&egrave; di pagina (collegamento), Icone strumenti di pagina, Tabbox - Colore del carattere';
+$lang['__nav_menu_hover_color__']   = 'Navigazione (passaggio) - Colore del carattere, Colore del bordo; Icone Wiki - Colore del carattere; Icone Wiki (passaggio) - Colore di sfondo';
+$lang['__nav_menu_hover_bg__']      = 'Navigazione (passaggio) - Colore di sfondo; Icone Wiki - Colore di sfondo; Icone Wiki (passaggio) - Colore del carattere';
+
+$lang['__background__']             = 'Contenuto, percorso, strumenti di pagina (passaggio) - Colore di sfondo';
+$lang['__text__']                   = 'Contenuto: Testo - Colore del carattere';
+
+$lang['__background_neu__']         = 'Contenuto neutro (non troppo diverso dallo sfondo del contenuto) - Colore di sfondo';
+$lang['__text_neu__']               = 'Contenuto neutro: Testo - Tolore del carattere';
+
+$lang['__background_page_header__'] = 'Contenuto: Intestazione, Pi&egrave; di pagina, Linguette tab disattivate per Tabinclude - Colore di sfondo';
+$lang['__background_page_footer__'] = 'Contenuto: Pi&egrave; di pagina, Linguette tab disattivate per Tabinclude - Colore del carattere';
+
+$lang['__border_light__']           = 'Contenuto: Intestazione, pi&egrave; di pagina - Colore del bordo';
+
+$lang['__highlight__']              = 'Enfasi del testo (ad esempio per i risultati di ricerca) - Colore di sfondo';
+$lang['__highlight_text__']         = 'Enfasi del testo (ad esempio per i risultati di ricerca) - Colore del carattere';
+
+$lang['__existing__']               = 'Collegamenti a pagine Wiki, matita, strumenti di pagina (passaggio) - Colore del carattere; Tabbox (passaggio) - Colore del carattere, Colore del bordo';
+$lang['__link__']                   = 'Collegamenti a pagine non Wiki (link esterni) - Colore del carattere';
+$lang['__missing__']                = 'Collegamenti a pagine non esistenti - Colore del carattere';
+
+$lang['__button_color__']           = 'Pulsanti - Colore del carattere';
+$lang['__button_background__']      = 'Pulsanti - Colore di sfondo';
+
+$lang['__background_alt__']         = 'Intestazione tabella, Cella tabella (passaggio), Etichetta Struct (passaggio) - Colore di sfondo (alternativo)';
+$lang['__text_alt__']               = 'Intestazione tabella: non collegata, Cella tabella (passaggio), Etichetta Struct (passaggio)- Colore del carattere (alternativo)';
+$lang['__link_alt__']               = 'Intestazione tabella: collegata - Colore del carattere (alternativo)';
+
+$lang['__border__']                 = 'Tabelle, Campi modulo, Blockquote - Colore del bordo';
+$lang['__default_border_radius__']  = 'Icone Wiki, Contenuto (in alto a destra) - raggio del bordo';
