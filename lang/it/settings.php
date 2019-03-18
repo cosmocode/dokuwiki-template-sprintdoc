2ee86fcfaa7f235cd4c3d5c27ff019e4def5fc68
Roberto Bellingeri <bellingeri@netguru.it>
2019-03-18 11:29:20 +0100

    Create settings.php

---
+++ lang/it/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * File di lingua Italiana per la pagina di configurazione
+ *
+ */
+
+
+$lang['sidebar_sections']   = 'Attiva/disattiva la barra laterale nelle sezioni su questi elementi:';
+$lang['sidebar_sections_o_h1']   = 'Intestazioni di primo ordine';
+$lang['sidebar_sections_o_h1,h2']   = 'Intestazioni di primo e secondo ordine';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Tutte le intestazioni';
+$lang['sidebar_sections_o_li.level1 > .li']   = 'Primo livello degli elementi di elenco';
+
+$lang['header_layout']   = 'Regola il layout dell\'intestazione:';
+$lang['header_layout_o_header-default']   = 'layout di intestazione generoso per un lungo titolo e una tagline';
+$lang['header_layout_o_header-compact']   = 'layout di intestazione compatto per un breve titolo e tagline nascosta';
+
