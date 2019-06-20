789be9bd00adae2bd601dfbd5f63df8215f74894
Henk <meinsma@hotmail.com>
2019-06-20 17:50:42 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Henk <meinsma@hotmail.com>
+ */
+$lang['direct_prefix']         = 'ga naar';
+$lang['direct_content_main']   = 'Hoofdinhoud';
+$lang['direct_quick_search']   = 'Zoeken';
+$lang['direct_content_toggle'] = 'Wijzig inhoud breedte';
+$lang['adjunct_linked_logo_text'] = 'link naar home pagina';
+$lang['a11y_sidebartoggle']    = 'Toon/Verberg navigatie';
+$lang['nav-area-head']         = 'Menu en snelzoeken';
+$lang['head_quick_search']     = 'Snelzoeken';
+$lang['head_menu_status']      = 'website status';
+$lang['head_menu_trace']       = 'Laatst bezochte pagina\'s';
+$lang['head_meta_box']         = 'meta data voor deze pagina';
+$lang['head_menu_starred']     = 'Gemarkeerde pagina\'s';
+$lang['head_menu_tool-select'] = 'Hulpmiddelen';
+$lang['jump_to_quicksearch']   = 'Ga naar snelzoeken';
+$lang['tab_tags']              = 'Tags';
+$lang['tab_issues']            = 'Onderwerpen';
+$lang['quality_trigger']       = 'Toon/Verberg pagina analyse';
+$lang['prefix_tasks_page']     = 'open taken voor deze pagina';
+$lang['tasks_page_none']       = 'geen openstaande taken voor deze pagina';
+$lang['tasks_page_intime']     = 'open taken op deze pagina: %1$d';
+$lang['tasks_page_late']       = 'open taken op deze pagina: %1$d, over tijd: %2$d';
+$lang['meta_data']             = 'meta data';
+$lang['osm_zoom_link_text']    = 'bekijk grotere kaart';
+$lang['osm_zoom_link_title']   = 'externe link opent in een nieuw venster';
+$lang['image_detail']          = 'afbeelding details';
+$lang['mode_edit']             = 'Bewerk pagina';
+$lang['nav_sidebar']           = 'Navigatie';
+$lang['nav_sitemap']           = 'Site structuur';
+$lang['__site_width__']        = 'inhoud - breedte (in %)';
+$lang['__background_site__']   = 'website - kleur achtergrond';
+$lang['__text_webframe__']     = 'claim, titel, footer (niet gelinkt), navigatie (niet gelinkt) - tekstkleur';
+$lang['__nav_menu_color__']    = 'navigatie (link), footer (link), pagina-hulpmiddelen iconen, tabbox, tekstkleur';
+$lang['__nav_menu_hover_color__'] = 'navigatie (hover), tekstkleur, kader kleur; wiki iconen - tekstkleur; wiki iconen (hover) - kleur achtergrond';
+$lang['__nav_menu_hover_bg__'] = 'navigatie (hover), kleur achtergrond; wiki iconen - kleur achtergrond; wiki iconen (hover) - tekstkleur';
+$lang['__background__']        = 'inhoud, broodkruimels, pagina hulpmiddelen (hover) - kleur achtergrond';
+$lang['__text__']              = 'inhoud: tekst - tekstkleur';
+$lang['__highlight__']         = 'tekst accentuering (bijv. voor zoekresultaten) - kleur achtergrond';
+$lang['__highlight_text__']    = 'tekst accentuering (bijv. voor zoekresultaten) - tekstkleur';
+$lang['__existing__']          = 'links naar wiki-pagina\'s, pen, pagina-hulpmiddelen (hover) - tekstkleur; tabbox (hover) - tekstkleur, kader kleur';
+$lang['__link__']              = 'links naar niet wiki-pagina\'s (externe links) - tekstkleur';
+$lang['__missing__']           = 'links naar niet niet bestaande pagina\'s (externe links) - tekstkleur';
+$lang['__button_color__']      = 'buttons - tekstkleur';
+$lang['__button_background__'] = 'buttons - kleur achtergrond';
