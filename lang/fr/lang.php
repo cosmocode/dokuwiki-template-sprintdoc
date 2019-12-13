e5bdc4703c234e7ad27ebae958957fb497fb417e
ISKANDER REKIK <rekik.iskander@gmail.com>
2019-12-13 21:55:09 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author ISKANDER REKIK <rekik.iskander@gmail.com>
+ */
+$lang['direct_prefix']         = 'sauter à ';
+$lang['direct_content_main']   = 'Contenu principal';
+$lang['direct_quick_search']   = 'Chercher';
+$lang['direct_content_toggle'] = 'Changer le Contenu avec';
+$lang['adjunct_linked_logo_text'] = '- lien page d\'accueil';
+$lang['a11y_sidebartoggle']    = 'Afficher/cacher Navigation';
+$lang['nav-area-head']         = 'menus et recherche rapide';
+$lang['head_magic_matcher']    = 'Correspondance Magique';
+$lang['head_quick_search']     = 'recherche rapide';
+$lang['head_menu_status']      = 'statut du site';
+$lang['head_breadcrumb_youarehere'] = 'indicateur de localisation';
+$lang['head_menu_trace']       = 'Pages récemment visitées';
+$lang['head_meta_box']         = 'meta données pour cette page';
+$lang['head_menu_starred']     = 'Pages suivies';
+$lang['head_menu_tool-select'] = 'Outils';
+$lang['jump_to_quicksearch']   = 'Sauter à la recherche rapide';
+$lang['tab_tags']              = 'Mots clés';
+$lang['tab_issues']            = 'problèmes';
+$lang['quality_trigger']       = 'Afficher/Cacher les analyses de la page';
+$lang['prefix_tasks_page']     = 'tâches ouvertes pour cette page:';
+$lang['tasks_page_none']       = 'pas de tâches ouvertes pour cette page';
+$lang['tasks_page_intime']     = 'tâches ouvertes sur cette page: %1$d';
+$lang['tasks_page_late']       = 'tâches ouvertes sur cette page: %1$d, sur %2$d';
+$lang['meta_data']             = 'Meta données';
+$lang['osm_zoom_link_text']    = 'Voire carte plus large';
+$lang['osm_zoom_link_title']   = 'lien externe ouvre une nouvelle fenêtre';
+$lang['image_detail']          = 'détails de l\'image';
+$lang['mode_edit']             = 'Page d\'édition';
+$lang['nav_sidebar']           = 'Navigation';
+$lang['nav_sitemap']           = 'Arborescence du site';
