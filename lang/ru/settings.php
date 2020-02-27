153d332c341c4d2102ba0d846e4211168ebccacb
ToLive <seventyseventh@gmail.com>
2020-02-27 15:22:31 +0200

    Added Russian language

---
+++ lang/ru/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Russian language file for config
+ *
+ */
+
+
+$lang['sidebar_sections']   = 'Автоматически переключать боковую панель в этих секциях:';
+$lang['sidebar_sections_o_h1']   = 'Заголовок H1';
+$lang['sidebar_sections_o_h1,h2']   = 'Заголовки H1 и H2';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Все заголовки';
+$lang['sidebar_sections_o_li.level1 > .li']   = 'Первые элементы в списках';
+
+$lang['header_layout']   = 'Настройка размещения заголовков:';
+$lang['header_layout_o_default']   = 'разреженное размещение длинных wiki-заголовков и блоков тегов';
+$lang['header_layout_o_compact']   = 'компактное размещение длинных wiki-заголовков и блоков тегов';
+
