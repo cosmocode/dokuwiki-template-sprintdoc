2e53d8b7d7f047362af7fc362fd492d9ba8d3eab
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-05-23 17:45:06 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,104 +1,75 @@
 <?php
+
 /**
  * Russian language file for template
  *
- *  @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
-
-
-$lang['direct_prefix']              = 'перейти к';
-$lang['direct_content_main']        = 'Главная страница';
-$lang['direct_quick_search']        = 'Поиск';
-$lang['direct_content_toggle']      = 'Изменить ширину рабочей области';
-
-$lang['adjunct_linked_logo_text']   = ' - homepage link';
-$lang['a11y_sidebartoggle']         = 'Показать\\скрыть навигацию';
-
-$lang['nav-area-head']              = 'меню и быстрый поиск';
-$lang['head_magic_matcher']         = 'MagicMatcher';
-$lang['head_quick_search']          = 'быстрый поиск';
-$lang['head_menu_status']           = 'статус сайта';
-$lang['head_breadcrumb_youarehere'] = 'хлебные крошки';
-$lang['head_menu_trace']            = 'Последние посещенные страницы';
-$lang['head_meta_box']              = 'мета данные страницы';
-$lang['head_menu_starred']          = 'Избранные страницы';
-$lang['head_menu_tool-select']      = 'Инструменты';
-
-$lang['jump_to_quicksearch']        = 'Переход к быстрому поиску';
-
-/** it will be shown from plugin lang.php:
-$lang['meta_box_jira_tickets_none'] = 'jira associated tickets found: none';
-$lang['meta_box_tags_none']         = 'tags found: none';
-$lang['js']['meta_box_toc_none']    = 'no Table of Contents available';
-$lang['prefix_tasks_user']          = 'Your open tasks: ';
-*/
-
-$lang['tab_tags']                   = 'Тэги';
-$lang['tab_issues']                 = 'Вопросы';
-
-$lang['quality_trigger']            = 'Показать\\скрыть аналитику';
-
-$lang['prefix_tasks_page']          = 'открыть задачи по странице: ';
-
-$lang['tasks_page_none']            = 'нет открытых задач по странице';
-$lang['tasks_page_intime']          = 'открыть задачи на странице: %1$d';
-$lang['tasks_page_late']            = 'открыть задачи на странице: %1$d, до: %2$d';
-
-/** is not used
-$lang['discussion']                 = 'Discussion';
-$lang['back_to_article']            = 'Back to article';
-$lang['userpage']                   = 'User page';
-*/
-
-$lang['meta_data']                  = 'Мета данные';
-$lang['osm_zoom_link_text']         = 'открыть расширенную карту';
-$lang['osm_zoom_link_title']        = 'внешние ссылки в новом окне';
-
-$lang['image_detail']               = 'информация по картинке';
-
-$lang['mode_edit']                  = 'Редактирование страницы';
-
-
-$lang['nav_sidebar']                = 'Навигация';
-$lang['nav_sitemap']                = 'Карта сайта';
-
-/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * it will be shown from plugin lang.php:
+ * $lang['meta_box_jira_tickets_none'] = 'jira associated tickets found: none';
+ * $lang['meta_box_tags_none']         = 'tags found: none';
+ * $lang['js']['meta_box_toc_none']    = 'no Table of Contents available';
+ * $lang['prefix_tasks_user']          = 'Your open tasks: ';
+ * is not used
+ * $lang['discussion']                 = 'Discussion';
+ * $lang['back_to_article']            = 'Back to article';
+ * $lang['userpage']                   = 'User page';
+ *
  * preferences colors
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-
-$lang['__site_width__']             = 'ширина области для контента (в %)';
-
-$lang['__background_site__']        = 'сайт - цвет фона';
-$lang['__text_webframe__']          = 'заглавие, подвал (без ссылок), навигация (без ссылок) - цвет шрифта';
-
-$lang['__nav_menu_color__']         = 'навигация (ссылка), подвал (ссылка), иконки инструментов страницы, панель вкладок - цвет шрифта';
-$lang['__nav_menu_hover_color__']   = 'навигация (при наведении) - цвет шрифта, border color; иконки wiki - цвет шрифта; иконки wiki (при наведении) - background color';
-$lang['__nav_menu_hover_bg__']      = 'навигация (при наведении) - цвет фона; иконки wiki - цвет фона; иконки wiki (при наведении) - цвет шрифта';
-
-$lang['__background__']             = 'содержимое, хлебные крошки, инструменты страницы (при наведении) - цвет фона';
-$lang['__text__']                   = 'содержимое: текст - цвет шрифта';
-
-$lang['__background_neu__']         = 'нейтральное содержимое (не сильно отличается от фонового содержимого) - цвет фона';
-$lang['__text_neu__']               = 'нейтральное содержимое: текст - цвет шрифта';
-
+$lang['direct_prefix']         = 'перейти к';
+$lang['direct_content_main']   = 'Главная страница';
+$lang['direct_quick_search']   = 'Поиск';
+$lang['direct_content_toggle'] = 'Изменить ширину рабочей области';
+$lang['adjunct_linked_logo_text'] = ' - homepage link';
+$lang['a11y_sidebartoggle']    = 'Показать/скрыть навигацию';
+$lang['nav-area-head']         = 'меню и быстрый поиск';
+$lang['head_magic_matcher']    = 'MagicMatcher';
+$lang['head_quick_search']     = 'быстрый поиск';
+$lang['head_menu_status']      = 'статус сайта';
+$lang['head_breadcrumb_youarehere'] = 'хлебные крошки';
+$lang['head_menu_trace']       = 'Последние посещённые страницы';
+$lang['head_meta_box']         = 'мета-данные страницы';
+$lang['head_menu_starred']     = 'Избранные страницы';
+$lang['head_menu_tool-select'] = 'Инструменты';
+$lang['jump_to_quicksearch']   = 'Переход к быстрому поиску';
+$lang['tab_tags']              = 'Теги';
+$lang['tab_issues']            = 'Вопросы';
+$lang['quality_trigger']       = 'Показать\скрыть аналитику';
+$lang['prefix_tasks_page']     = 'открыть задачи по странице: ';
+$lang['tasks_page_none']       = 'нет открытых задач по странице';
+$lang['tasks_page_intime']     = 'открыть задачи на странице: %1$d';
+$lang['tasks_page_late']       = 'открыть задачи на странице: %1$d, до: %2$d';
+$lang['meta_data']             = 'Мета-данные';
+$lang['osm_zoom_link_text']    = 'открыть расширенную карту';
+$lang['osm_zoom_link_title']   = 'внешние ссылки в новом окне';
+$lang['image_detail']          = 'информация по картинке';
+$lang['mode_edit']             = 'Редактирование страницы';
+$lang['nav_sidebar']           = 'Навигация';
+$lang['nav_sitemap']           = 'Карта сайта';
+$lang['__site_width__']        = 'ширина области для контента (в %)';
+$lang['__background_site__']   = 'сайт - цвет фона';
+$lang['__text_webframe__']     = 'заглавие, подвал (без ссылок), навигация (без ссылок) - цвет шрифта';
+$lang['__nav_menu_color__']    = 'навигация (ссылка), подвал (ссылка), иконки инструментов страницы, панель вкладок - цвет шрифта';
+$lang['__nav_menu_hover_color__'] = 'навигация (при наведении) - цвет шрифта, цвет границы; иконки вики - цвет шрифта; иконки вики (при наведении) - цвет фона';
+$lang['__nav_menu_hover_bg__'] = 'навигация (при наведении) - цвет фона; иконки вики - цвет фона; иконки вики (при наведении) - цвет шрифта';
+$lang['__background__']        = 'содержимое, хлебные крошки, инструменты страницы (при наведении) - цвет фона';
+$lang['__text__']              = 'содержимое: текст - цвет шрифта';
+$lang['__background_neu__']    = 'нейтральное содержимое (не сильно отличается от фонового содержимого) - цвет фона';
+$lang['__text_neu__']          = 'нейтральное содержимое: текст - цвет шрифта';
 $lang['__background_page_header__'] = 'содержимое: заголовок, подвал, tabinclude tab controlls deactivate - цвет фона';
 $lang['__background_page_footer__'] = 'содержимое: подвал, tabinclude tab controlls deactivate - цвет шрифта';
-
-$lang['__border_light__']           = 'содержимое: заголовок, подвал - цвет границы';
-
-$lang['__highlight__']              = 'подчеркнутый текст (например, результаты поиска) - цвет фона';
-$lang['__highlight_text__']         = 'подчеркнутый текст (например, результаты поиска) - цвет шрифта';
-
-$lang['__existing__']               = 'ссылки на wiki-страницы, карандаш, инструменты страницы (при наведении) - цвет шрифта; панель вкладок (при наведении) - цвет шрифта, цвет границы';
-$lang['__link__']                   = 'ссылки на внешние страницы - цвет шрифта';
-$lang['__missing__']                = 'ссылки на не существующие страницы - цвет шрифта';
-
-$lang['__button_color__']           = 'кнопки - цвет шрифта';
-$lang['__button_background__']      = 'кнопки - цвет фона';
-
-$lang['__background_alt__']         = 'заголовок таблицы, ячейка таблицы (при наведении), ярлык структуры (при наведении) - цвет фона (альтернативный)';
-$lang['__text_alt__']               = 'заголовок таблицы: без ссылки, ячейка таблицы (при наведении), ярлык структуры (при наведении) - цвет шрифта (alternative)';
-$lang['__link_alt__']               = 'заголовок таблицы: связанная - цвет шрифта (альтернативный)';
-
-$lang['__border__']                 = 'таблицы, поля форм, цитаты - цвет границы';
-$lang['__default_border_radius__']  = 'иконки wiki, содержимое (верхний правый угол) - скругление углов границ';
+$lang['__border_light__']      = 'содержимое: заголовок, подвал - цвет границы';
+$lang['__highlight__']         = 'подчеркнутый текст (например, результаты поиска) - цвет фона';
+$lang['__highlight_text__']    = 'подчеркнутый текст (например, результаты поиска) - цвет шрифта';
+$lang['__existing__']          = 'ссылки на вики-страницы, карандаш, инструменты страницы (при наведении) - цвет шрифта; панель вкладок (при наведении) - цвет шрифта, цвет границы';
+$lang['__link__']              = 'ссылки на внешние страницы - цвет шрифта';
+$lang['__missing__']           = 'ссылки на не существующие страницы - цвет шрифта';
+$lang['__button_color__']      = 'кнопки - цвет шрифта';
+$lang['__button_background__'] = 'кнопки - цвет фона';
+$lang['__background_alt__']    = 'заголовок таблицы, ячейка таблицы (при наведении), ярлык структуры (при наведении) - цвет фона (альтернативный)';
+$lang['__text_alt__']          = 'заголовок таблицы: без ссылки, ячейка таблицы (при наведении), ярлык структуры (при наведении) - цвет шрифта (alternative)';
+$lang['__link_alt__']          = 'заголовок таблицы: связанная - цвет шрифта (альтернативный)';
+$lang['__border__']            = 'таблицы, поля форм, цитаты - цвет границы';
+$lang['__default_border_radius__'] = 'иконки вики, содержимое (верхний правый угол) - скругление углов границ';
