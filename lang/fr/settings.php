08356d264e5e7cd512c27b2fb947ba673b4524e7
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2020-08-22 19:15:38 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ */
+$lang['sidebar_sections']      = 'Convertir automatique les en sections ces éléments :';
+$lang['sidebar_sections_o_h1'] = 'titres de premier rang';
+$lang['sidebar_sections_o_h1,h2'] = 'titres de premier et second rang';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5'] = 'tous les titres';
+$lang['sidebar_sections_o_li.level1 > .li'] = 'items de premier niveau des listes';
+$lang['header_layout']         = 'Ajuster la disposition de l\'entête :';
+$lang['header_layout_o_default'] = 'Disposition d\'entête généreuse pour un titre long et un descriptif';
+$lang['header_layout_o_compact'] = 'Disposition compacte de l\'entête pour un titre court sans descriptif';
