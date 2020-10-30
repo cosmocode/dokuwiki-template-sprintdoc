2710cc95d8cd175d44c258f33a8d4199b2c3cf5b
GHSRobert Ciang <robertus0617@gmail.com>
2020-10-30 07:05:06 +0100

    translation update

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author GHSRobert Ciang <robertus0617@gmail.com>
+ */
+$lang['head_menu_trace']       = '最近瀏覽的頁面';
+$lang['head_menu_tool-select'] = '工具';
+$lang['tab_tags']              = '標籤';
+$lang['quality_trigger']       = '顯示/隱藏頁面分析';
+$lang['prefix_tasks_page']     = '此頁尚未完成的工作：';
+$lang['tasks_page_none']       = '此頁工作都已完成';
