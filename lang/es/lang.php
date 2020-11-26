9cf1b31d2689811bc21c7ca8cdb6963004a13516
Gustavo Benjamin <gustavo@wsd10.valsol.net>
2020-11-26 17:36:29 -0300

    copy en lang files as es lang files

---
+++ lang/es/lang.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * English language file for template
+ *
+ *  @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+
+$lang['direct_prefix']              = 'jump to';
+$lang['direct_content_main']        = 'Main Content';
+$lang['direct_quick_search']        = 'Search';
+$lang['direct_content_toggle']      = 'Change Content Width';
+
+$lang['adjunct_linked_logo_text']   = ' - homepage link';
+$lang['a11y_sidebartoggle']         = 'Show/Hide Navigation';
+
+$lang['nav-area-head']              = 'menus and quick search';
+$lang['head_magic_matcher']         = 'MagicMatcher';
+$lang['head_quick_search']          = 'quick search';
+$lang['head_menu_status']           = 'site status';
+$lang['head_breadcrumb_youarehere'] = 'location indicator';
+$lang['head_menu_trace']            = 'Last Visited Pages';
+$lang['head_meta_box']              = 'meta data for this page';
+$lang['head_menu_starred']          = 'Starred Pages';
+$lang['head_menu_tool-select']      = 'Tools';
+
+$lang['jump_to_quicksearch']        = 'Jump to quick search';
+
+/** it will be shown from plugin lang.php:
+$lang['meta_box_jira_tickets_none'] = 'jira associated tickets found: none';
+$lang['meta_box_tags_none']         = 'tags found: none';
+$lang['js']['meta_box_toc_none']    = 'no Table of Contents available';
+$lang['prefix_tasks_user']          = 'Your open tasks: ';
+*/
+
+$lang['tab_tags']                   = 'Tags';
+$lang['tab_issues']                 = 'Issues';
+
+$lang['quality_trigger']            = 'Show/Hide page analysis';
+
+$lang['prefix_tasks_page']          = 'open tasks for this page: ';
+
+$lang['tasks_page_none']            = 'no open tasks on this page';
+$lang['tasks_page_intime']          = 'open tasks on this page: %1$d';
+$lang['tasks_page_late']            = 'open tasks on this page: %1$d, to late: %2$d';
+
+/** is not used
+$lang['discussion']                 = 'Discussion';
+$lang['back_to_article']            = 'Back to article';
+$lang['userpage']                   = 'User page';
+*/
+
+$lang['meta_data']                  = 'Meta Data';
+$lang['osm_zoom_link_text']         = 'view larger map';
+$lang['osm_zoom_link_title']        = 'external link opens new window';
+
+$lang['image_detail']               = 'image details';
+
+$lang['mode_edit']                  = 'Editing Page';
+
+
+$lang['nav_sidebar']                = 'Navigation';
+$lang['nav_sitemap']                = 'Site Tree';
+
+/**
+ * preferences colors
+ */
+
+$lang['__site_width__']             = 'content - width (in %)';
+
+$lang['__background_site__']        = 'website - background color';
+$lang['__text_webframe__']          = 'claim, title, footer (unlinked), navigation (unlinked) - font color';
+
+$lang['__nav_menu_color__']         = 'navigation (link), footer (link), pagetools icons, tabbox - font color';
+$lang['__nav_menu_hover_color__']   = 'navigation (hover) - font color, border color; wiki icons - font color; wiki icons (hover) - background color';
+$lang['__nav_menu_hover_bg__']      = 'navigation (hover) - background color; wiki icons - background color; wiki icons (hover) - font color';
+
+$lang['__background__']             = 'content, breadcrumb, pagetools (hover) - background color';
+$lang['__text__']                   = 'content: text - font color';
+
+$lang['__background_neu__']         = 'Content neutral (not too different from the content background) - background color';
+$lang['__text_neu__']               = 'content neutral: text - font color';
+
+$lang['__background_page_header__'] = 'content: header, footer, tabinclude tab controlls deactivate - background color';
+$lang['__background_page_footer__'] = 'content: footer, tabinclude tab controlls deactivate - font color';
+
+$lang['__border_light__']           = 'content: header, footer - border color';
+
+$lang['__highlight__']              = 'text emphasize (e.g. for search results) - background color';
+$lang['__highlight_text__']         = 'text emphasize (e.g. for search results) - font color';
+
+$lang['__existing__']               = 'links to wikipages, pencil, pagetools (hover) - font color; tabbox (hover) - font color, border color';
+$lang['__link__']                   = 'links to non wikipages (external links) - font color';
+$lang['__missing__']                = 'links to non-existing pages - font color';
+
+$lang['__button_color__']           = 'buttons - font color';
+$lang['__button_background__']      = 'buttons - background color';
+
+$lang['__background_alt__']         = 'table head, table cell (hover), struct Label (hover) - background color (alternative)';
+$lang['__text_alt__']               = 'table head: unlinked, table cell (hover), struct Label (hover) - font color (alternative)';
+$lang['__link_alt__']               = 'table head: linked - font color (alternative)';
+
+$lang['__border__']                 = 'tables, form fields, blockquotes - border color';
+$lang['__default_border_radius__']  = 'wiki icons, content (top right) - border radius';
---
+++ lang/es/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * English language file for config
+ *
+ */
+
+
+$lang['sidebar_sections']   = 'Auto-toggle the sidebar into sections on these elements:';
+$lang['sidebar_sections_o_h1']   = '1st order headings';
+$lang['sidebar_sections_o_h1,h2']   = '1st and 2nd order headings';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'All headings';
+$lang['sidebar_sections_o_li.level1 > .li']   = 'Top-level list items';
+
+$lang['header_layout']   = 'Adjust header layout:';
+$lang['header_layout_o_default']   = 'generous header layout for long wiki titel and tagline';
+$lang['header_layout_o_compact']   = 'compact header layout for short wiki title and hidden tagline';
+
