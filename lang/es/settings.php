73a5585ab77166fb6ae47759d108a03ab57b83e8
Gustavo Benjamin <gustavo@wsd10.valsol.net>
2020-12-06 19:14:09 -0300

    samll spanish language typos

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -12,6 +12,5 @@ $lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Todos los encabezados';
 $lang['sidebar_sections_o_li.level1 > .li']   = 'Items del mayor nivel en listas';
 
 $lang['header_layout']   = 'Adjust header layout:';
-$lang['header_layout_o_default']   = 'disposición generosa del encabezado para largos títulos y lemas de la wiki';
-$lang['header_layout_o_compact']   = 'disposición compacta del encabezado para títulos cortos de la wiki y lema oculto';
-
+$lang['header_layout_o_default']   = 'disposición generosa del encabezado (para título y lema de la wiki largos)';
+$lang['header_layout_o_compact']   = 'disposición compacta del encabezado (para título corto de la wiki y lema oculto)';
