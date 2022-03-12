9c07a606d5a6b498697d22d0269a837aabd26b25
Ben_2003 <dsoeteber@web.de>
2022-03-12 15:45:19 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,105 +1,76 @@
 <?php
+
 /**
  * German language file for template
- *__background_site__
+ * __background_site__
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
-
-
-$lang['direct_prefix']              = 'Direkt';
-$lang['direct_content_main']        = 'Hauptinhalt dieser Seite';
-$lang['direct_quick_search']        = 'Schnellsuche';
-$lang['direct_content_toggle']      = 'Breite des Contents ändern';
-
-$lang['adjunct_linked_logo_text']   = ' - Link zur Startseite';
-$lang['a11y_sidebartoggle']         = 'Navigation anzeigen/verbergen';
-
-$lang['nav-area-head']              = 'Navigationsmenüs und Suche';
-$lang['head_magic_matcher']         = 'MagicMatcher';
-$lang['head_quick_search']          = 'Wikiübergreifende Schnellsuche';
-$lang['head_menu_status']           = 'Seitenstatus';
-$lang['head_breadcrumb_youarehere'] = 'Standortanzeiger';
-$lang['head_menu_trace']            = 'Zuletzt Angesehen';
-$lang['head_meta_box']              = 'Metainformationen zur Seite';
-$lang['head_menu_starred']          = 'Gemerkte Seiten';
-$lang['head_menu_tool-select']      = 'Werkzeuge';
-
-$lang['jump_to_quicksearch']        = 'Springe zur Schnellsuche';
-
-/** it will be shown from plugin lang.php:
-$lang['meta_box_jira_tickets_none'] = 'Keine Jira-Tickets verknüpft';
-$lang['meta_box_tags_none']         = 'Keine Tags verknüpft';
-$lang['js']['meta_box_toc_none']    = 'kein Inhaltsverzeichnis vorhanden';
-$lang['prefix_tasks_user']          = 'Ihre offenen Aufgaben: ';
-*/
-
-$lang['tab_tags']                   = 'Tags';
-$lang['tab_issues']                 = 'Issues';
-
-$lang['quality_trigger']            = 'Seitenanalyse anzeigen/verbergen';
-
-$lang['prefix_tasks_page']          = 'offene Aufgaben auf dieser Seite: ';
-
-$lang['tasks_page_none']            = 'keine offenen Aufgaben für diese Seite';
-$lang['tasks_page_intime']          = 'offenen Aufgaben für diese Seite: %1$d';
-$lang['tasks_page_late']            = 'offenen Aufgaben für diese Seite: %1$d, zu spät: %2$d';
-
-/** is not used
-$lang['discussion']                 = 'xxx Diskussion';
-$lang['back_to_article']            = 'xxx Zurück zum Artikel';
-$lang['userpage']                   = 'xxx Benutzer-Seite';
- */
-
-$lang['meta_data']                  = 'Meta Daten des Bildes'; /** wird aufgerufen aber nicht ausgegeben - Fehler? */
-$lang['osm_zoom_link_text']         = 'größere Karte anzeigen';
-$lang['osm_zoom_link_title']        = 'externer Link - öffnet in neuem Fenster'; /** in streetmaps - konnte ich nicht finden */
-
-$lang['image_detail']               = 'Detailinformationen zum Bild';
-
-$lang['mode_edit']                  = 'Seite bearbeiten';
-
-
-$lang['nav_sidebar']                = 'Navigation';
-$lang['nav_sitemap']                = 'Seitenbaum';
-
-
-/**
+ * it will be shown from plugin lang.php:
+ * $lang['meta_box_jira_tickets_none'] = 'Keine Jira-Tickets verknüpft';
+ * $lang['meta_box_tags_none']         = 'Keine Tags verknüpft';
+ * $lang['js']['meta_box_toc_none']    = 'kein Inhaltsverzeichnis vorhanden';
+ * $lang['prefix_tasks_user']          = 'Ihre offenen Aufgaben: ';
+ * is not used
+ * $lang['discussion']                 = 'xxx Diskussion';
+ * $lang['back_to_article']            = 'xxx Zurück zum Artikel';
+ * $lang['userpage']                   = 'xxx Benutzer-Seite';
+ * wird aufgerufen aber nicht ausgegeben - Fehler? 
+ * in streetmaps - konnte ich nicht finden 
+ *
  * preferences colors
+ *
  */
-
-$lang['__site_width__']             = 'Content - Breite (in %)';
-
-$lang['__background_site__']        = 'Website - Hintergrundfarbe';
-$lang['__text_webframe__']          = 'Claim, Titel, Footer (unverlinkt), Navigation (unverlinkt) - Schriftfarbe';
-
-$lang['__nav_menu_color__']         = 'Navigation (verlinkt), Footer (verlinkt), Pagetools Icons, Tabbox - Schriftfarbe';
-$lang['__nav_menu_hover_color__']   = 'Navigation (hover) - Schriftfarbe, Rahmenfarbe; Wiki Icons - Schriftfarbe; Wiki Icons (hover) - Hintergrundfarbe';
-$lang['__nav_menu_hover_bg__']      = 'Navigation (hover) - Hintergrundfarbe; Wiki Icons - Hintergrundfarbe; Wiki Icons (hover) - Schriftfarbe';
-
-$lang['__background__']             = 'Content, Breadcrumb, Pagetools (hover) - Hintergrundfarbe';
-$lang['__text__']                   = 'Content: Text - Schriftfarbe';
-
-$lang['__background_neu__']         = 'Content neutral (nicht zu unterschiedlich zum Content-Hintergrund) - Hintergrundfarbe';
-$lang['__text_neu__']               = 'Content neutral - Schriftfarbe';
-
+$lang['direct_prefix']         = 'Direkt';
+$lang['direct_content_main']   = 'Hauptinhalt dieser Seite';
+$lang['direct_quick_search']   = 'Schnellsuche';
+$lang['direct_content_toggle'] = 'Breite des Contents ändern';
+$lang['adjunct_linked_logo_text'] = ' - Link zur Startseite';
+$lang['a11y_sidebartoggle']    = 'Navigation anzeigen/verbergen';
+$lang['nav-area-head']         = 'Navigationsmenüs und Suche';
+$lang['head_magic_matcher']    = 'MagicMatcher';
+$lang['head_quick_search']     = 'Wikiübergreifende Schnellsuche';
+$lang['head_menu_status']      = 'Seitenstatus';
+$lang['head_breadcrumb_youarehere'] = 'Standortanzeiger';
+$lang['head_menu_trace']       = 'Zuletzt Angesehen';
+$lang['head_meta_box']         = 'Metainformationen zur Seite';
+$lang['head_menu_starred']     = 'Gemerkte Seiten';
+$lang['head_menu_tool-select'] = 'Werkzeuge';
+$lang['jump_to_quicksearch']   = 'Springe zur Schnellsuche';
+$lang['tab_tags']              = 'Tags';
+$lang['tab_issues']            = 'Issues';
+$lang['quality_trigger']       = 'Seitenanalyse anzeigen/verbergen';
+$lang['prefix_tasks_page']     = 'offene Aufgaben auf dieser Seite: ';
+$lang['tasks_page_none']       = 'keine offenen Aufgaben für diese Seite';
+$lang['tasks_page_intime']     = 'offenen Aufgaben für diese Seite: %1$d';
+$lang['tasks_page_late']       = 'offenen Aufgaben für diese Seite: %1$d, zu spät: %2$d';
+$lang['meta_data']             = 'Meta Daten des Bildes';
+$lang['osm_zoom_link_text']    = 'größere Karte anzeigen';
+$lang['osm_zoom_link_title']   = 'externer Link - öffnet in neuem Fenster';
+$lang['image_detail']          = 'Detailinformationen zum Bild';
+$lang['mode_edit']             = 'Seite bearbeiten';
+$lang['nav_sidebar']           = 'Navigation';
+$lang['nav_sitemap']           = 'Seitenbaum';
+$lang['__site_width__']        = 'Content - Breite (in %)';
+$lang['__background_site__']   = 'Website - Hintergrundfarbe';
+$lang['__text_webframe__']     = 'Claim, Titel, Footer (unverlinkt), Navigation (unverlinkt) - Schriftfarbe';
+$lang['__nav_menu_color__']    = 'Navigation (verlinkt), Footer (verlinkt), Pagetools Icons, Tabbox - Schriftfarbe';
+$lang['__nav_menu_hover_color__'] = 'Navigation (hover) - Schriftfarbe, Rahmenfarbe; Wiki Icons - Schriftfarbe; Wiki Icons (hover) - Hintergrundfarbe';
+$lang['__nav_menu_hover_bg__'] = 'Navigation (hover) - Hintergrundfarbe; Wiki Icons - Hintergrundfarbe; Wiki Icons (hover) - Schriftfarbe';
+$lang['__background__']        = 'Content, Breadcrumb, Pagetools (hover) - Hintergrundfarbe';
+$lang['__text__']              = 'Content: Text - Schriftfarbe';
+$lang['__background_neu__']    = 'Content neutral (nicht zu unterschiedlich zum Content-Hintergrund) - Hintergrundfarbe';
+$lang['__text_neu__']          = 'Content neutral - Schriftfarbe';
 $lang['__background_page_header__'] = 'Content: Header, Footer, Tabinclude Tab controlls deaktiviert - Hintergrundfarbe';
 $lang['__background_page_footer__'] = 'Content: Footer, Tabinclude Tab controlls deaktiviert - Schriftfarbe';
-
-$lang['__border_light__']           = 'Content: Header, Footer - Rahmenfarbe';
-
-$lang['__highlight__']              = 'Texthervorhebung (z.B. für Suchergebnisse) - Hintergrundfarbe';
-$lang['__highlight_text__']         = 'Texthervorhebung (z.B. für Suchergebnisse) - Schriftfarbe';
-
-$lang['__existing__']               = 'Links zu Wikiseiten, Stift, Pagetools (hover) - Schriftfarbe; Tabbox (hover) - Schriftfarbe, Rahmenfarbe';
-$lang['__link__']                   = 'Links zu nicht-Wikiseiten (externe Links) - Schriftfarbe';
-$lang['__missing__']                = 'Links zu nicht vorhandenen Wikiseiten - Schriftfarbe';
-
-$lang['__button_color__']           = 'Buttons - Schriftfarbe';
-$lang['__button_background__']      = 'Buttons - Hintergrundfarbe';
-
-$lang['__background_alt__']         = 'Tabellenkopf, Tabellenzeile (hover), struct Label (hover) - Hintergrundfarbe (alternativ)';
-$lang['__text_alt__']               = 'Tabellenkopf: unverlinkt, Tabellenzeile (hover), struct Label (hover) - Schriftfarbe (alternativ)';
-$lang['__link_alt__']               = 'Tabellenkopf: verlinkt - Schriftfarbe (alternativ)';
-
-$lang['__border__']                 = 'Tabellen, Formularfelder, Zitate - Rahmenfarbe';
-$lang['__default_border_radius__']  = 'Wiki Icons, Content (oben rechts) - Borderradius';
+$lang['__border_light__']      = 'Content: Header, Footer - Rahmenfarbe';
+$lang['__highlight__']         = 'Texthervorhebung (z.B. für Suchergebnisse) - Hintergrundfarbe';
+$lang['__highlight_text__']    = 'Texthervorhebung (z.B. für Suchergebnisse) - Schriftfarbe';
+$lang['__existing__']          = 'Links zu Wikiseiten, Stift, Pagetools (hover) - Schriftfarbe; Tabbox (hover) - Schriftfarbe, Rahmenfarbe';
+$lang['__link__']              = 'Links zu nicht-Wikiseiten (externe Links) - Schriftfarbe';
+$lang['__missing__']           = 'Links zu nicht vorhandenen Wikiseiten - Schriftfarbe';
+$lang['__button_color__']      = 'Buttons - Schriftfarbe';
+$lang['__button_background__'] = 'Buttons - Hintergrundfarbe';
+$lang['__background_alt__']    = 'Tabellenkopf, Tabellenzeile (hover), struct Label (hover) - Hintergrundfarbe (alternativ)';
+$lang['__text_alt__']          = 'Tabellenkopf: unverlinkt, Tabellenzeile (hover), struct Label (hover) - Schriftfarbe (alternativ)';
+$lang['__link_alt__']          = 'Tabellenkopf: verlinkt - Schriftfarbe (alternativ)';
+$lang['__border__']            = 'Tabellen, Formularfelder, Zitate - Rahmenfarbe';
+$lang['__default_border_radius__'] = 'Wiki Icons, Content (oben rechts) - Borderradius';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * Deutsche Übersetzung Einstellungen
  *
  */
-
-
-$lang['sidebar_sections']   = 'Automatisches Ein- und Ausblenden der Sidebar Abschnitte orientieren sich an diesen Elementen:';
-$lang['sidebar_sections_o_h1']   = 'Überschriften 1. Ordnung';
-$lang['sidebar_sections_o_h1,h2']   = 'Überschriften der 1. und 2. Ordnung';
-$lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Alle Überschriften';
-$lang['sidebar_sections_o_li.level1 > .li']   = 'Top-level Listeneinträge in verschachtelten Listen';
-
-$lang['header_layout']   = 'Layout des Headers anpassen:';
-$lang['header_layout_o_default']   = 'großer Header für langen Wiki Titel und Tagline';
-$lang['header_layout_o_compact']   = 'kompakter Header für kurzen Wiki Titel ohne Tagline';
-
-$lang['autocollapse'] = 'Sidebar automatisch einklappen wenn Nicht-Inhaltsseiten angezigt werden, um mehr Platz zu haben.';
+$lang['sidebar_sections']      = 'Automatisches Ein- und Ausblenden der Sidebar Abschnitte orientieren sich an diesen Elementen:';
+$lang['sidebar_sections_o_h1'] = 'Überschriften 1. Ordnung';
+$lang['sidebar_sections_o_h1,h2'] = 'Überschriften der 1. und 2. Ordnung';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5'] = 'Alle Überschriften';
+$lang['sidebar_sections_o_li.level1 > .li'] = 'Top-level Listeneinträge in verschachtelten Listen';
+$lang['header_layout']         = 'Layout des Headers anpassen:';
+$lang['header_layout_o_default'] = 'großer Header für langen Wiki Titel und Tagline';
+$lang['header_layout_o_compact'] = 'kompakter Header für kurzen Wiki Titel ohne Tagline';
+$lang['autocollapse']          = 'Sidebar automatisch einklappen wenn Nicht-Inhaltsseiten angezigt werden, um mehr Platz zu haben.';
