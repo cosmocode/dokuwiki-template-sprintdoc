ae95b51015b409f8116b231a9c6b70110bc76f50
law essay help <jacquelynmacaluso@live.co.uk>
2025-01-25 15:40:34 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,105 +1,110 @@
 <?php
+
 /**
  * German language file for template
- *__background_site__
+ * __background_site__
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
-
-
-$lang['direct_prefix']              = 'Direkt';
-$lang['direct_content_main']        = 'Hauptinhalt dieser Seite';
-$lang['direct_quick_search']        = 'Schnellsuche';
-$lang['direct_content_toggle']      = 'Breite des Contents ändern';
-
-$lang['adjunct_linked_logo_text']   = ' - Link zur Startseite';
-$lang['a11y_sidebartoggle']         = 'Navigation anzeigen/verbergen';
-
-$lang['nav-area-head']              = 'Navigationsmenüs und Suche';
-$lang['head_magic_matcher']         = 'MagicMatcher';
-$lang['head_quick_search']          = 'Wikiübergreifende Schnellsuche';
-$lang['head_menu_status']           = 'Seitenstatus';
-$lang['head_breadcrumb_youarehere'] = 'Standortanzeiger';
-$lang['head_menu_trace']            = 'Zuletzt Angesehen';
-$lang['head_meta_box']              = 'Metainformationen zur Seite';
-$lang['head_menu_starred']          = 'Gemerkte Seiten';
-$lang['head_menu_tool-select']      = 'Werkzeuge';
-
-$lang['jump_to_quicksearch']        = 'Springe zur Schnellsuche';
-
-/** it will be shown from plugin lang.php:
-$lang['meta_box_jira_tickets_none'] = 'Keine Jira-Tickets verknüpft';
-$lang['meta_box_tags_none']         = 'Keine Tags verknüpft';
-$lang['js']['meta_box_toc_none']    = 'kein Inhaltsverzeichnis vorhanden';
-$lang['prefix_tasks_user']          = 'Ihre offenen Aufgaben: ';
-*/
-
-$lang['tab_tags']                   = 'Tags';
-$lang['tab_issues']                 = 'Issues';
-
-$lang['quality_trigger']            = 'Seitenanalyse anzeigen/verbergen';
-
-$lang['prefix_tasks_page']          = 'offene Aufgaben auf dieser Seite: ';
-
-$lang['tasks_page_none']            = 'keine offenen Aufgaben für diese Seite';
-$lang['tasks_page_intime']          = 'offenen Aufgaben für diese Seite: %1$d';
-$lang['tasks_page_late']            = 'offenen Aufgaben für diese Seite: %1$d, zu spät: %2$d';
-
-/** is not used
-$lang['discussion']                 = 'xxx Diskussion';
-$lang['back_to_article']            = 'xxx Zurück zum Artikel';
-$lang['userpage']                   = 'xxx Benutzer-Seite';
- */
-
-$lang['meta_data']                  = 'Meta Daten des Bildes'; /** wird aufgerufen aber nicht ausgegeben - Fehler? */
-$lang['osm_zoom_link_text']         = 'größere Karte anzeigen';
-$lang['osm_zoom_link_title']        = 'externer Link - öffnet in neuem Fenster'; /** in streetmaps - konnte ich nicht finden */
-
-$lang['image_detail']               = 'Detailinformationen zum Bild';
-
-$lang['mode_edit']                  = 'Seite bearbeiten';
-
-
-$lang['nav_sidebar']                = 'Navigation';
-$lang['nav_sitemap']                = 'Seitenbaum';
-
-
-/**
+ * it will be shown from plugin lang.php:
+ * $lang['meta_box_jira_tickets_none'] = 'Keine Jira-Tickets verknüpft';
+ * $lang['meta_box_tags_none']         = 'Keine Tags verknüpft';
+ * $lang['js']['meta_box_toc_none']    = 'kein Inhaltsverzeichnis vorhanden';
+ * $lang['prefix_tasks_user']          = 'Ihre offenen Aufgaben: ';
+ * is not used
+ * $lang['discussion']                 = 'xxx Diskussion';
+ * $lang['back_to_article']            = 'xxx Zurück zum Artikel';
+ * $lang['userpage']                   = 'xxx Benutzer-Seite';
+ * wird aufgerufen aber nicht ausgegeben - Fehler? 
+ * in streetmaps - konnte ich nicht finden 
+ *
  * preferences colors
+ *
+ * @author law essay help <jacquelynmacaluso@live.co.uk>
  */
+$lang['direct_prefix']         = 'Direkt';
+$lang['direct_content_main']   = 'You could certainly see your enthusiasm within the article you write.
 
-$lang['__site_width__']             = 'Content - Breite (in %)';
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how 
+they believe. Always go after your heart.';
+$lang['direct_quick_search']   = 'Schnellsuche';
+$lang['direct_content_toggle'] = 'You could certainly see your enthusiasm within the article you 
+write. The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they believe.
+Always go after your heart.';
+$lang['adjunct_linked_logo_text'] = 'https://topratedstuff.net/law_essay_help.html';
+$lang['a11y_sidebartoggle']    = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they 
+believe. Always go after your heart.';
+$lang['nav-area-head']         = 'Im glad I finally registered';
+$lang['head_magic_matcher']    = 'Im glad I finally registered';
+$lang['head_quick_search']     = 'Wikiübergreifende Schnellsuche';
+$lang['head_menu_status']      = 'Im glad I finally registered';
+$lang['head_breadcrumb_youarehere'] = 'Im glad I finally registered';
+$lang['head_menu_trace']       = 'Im glad I finally registered';
+$lang['head_meta_box']         = 'Im glad I finally registered';
+$lang['head_menu_starred']     = 'Im glad I finally registered';
+$lang['head_menu_tool-select'] = 'Im glad I finally registered';
+$lang['jump_to_quicksearch']   = 'Springe zur Schnellsuche';
+$lang['tab_tags']              = '18';
+$lang['tab_issues']            = 'You could certainly see your enthusiasm within the 
+article you write. The sector hopes for more passionate writers such as you who 
+aren\'t afraid to mention how they believe. Always go after your heart.';
+$lang['quality_trigger']       = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you who aren\'t 
+afraid to mention how they believe. Always go after your heart.';
+$lang['prefix_tasks_page']     = 'offene Aufgaben auf dieser Seite:';
+$lang['tasks_page_none']       = 'You could certainly see your enthusiasm within the article 
+you write. The sector hopes for more passionate writers such 
+as you who aren\'t afraid to mention how they believe.
 
-$lang['__background_site__']        = 'Website - Hintergrundfarbe';
-$lang['__text_webframe__']          = 'Claim, Titel, Footer (unverlinkt), Navigation (unverlinkt) - Schriftfarbe';
+Always go after your heart.';
+$lang['tasks_page_intime']     = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they 
+believe. Always go after your heart.';
+$lang['tasks_page_late']       = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they 
+believe. Always go after your heart.';
+$lang['meta_data']             = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they believe.
+Always go after your heart.';
+$lang['osm_zoom_link_text']    = 'https://topratedstuff.net/law_essay_help.html';
+$lang['osm_zoom_link_title']   = 'https://topratedstuff.net/law_essay_help.html';
+$lang['image_detail']          = 'You could certainly see your enthusiasm within the article you write.
 
-$lang['__nav_menu_color__']         = 'Navigation (verlinkt), Footer (verlinkt), Pagetools Icons, Tabbox - Schriftfarbe';
-$lang['__nav_menu_hover_color__']   = 'Navigation (hover) - Schriftfarbe, Rahmenfarbe; Wiki Icons - Schriftfarbe; Wiki Icons (hover) - Hintergrundfarbe';
-$lang['__nav_menu_hover_bg__']      = 'Navigation (hover) - Hintergrundfarbe; Wiki Icons - Hintergrundfarbe; Wiki Icons (hover) - Schriftfarbe';
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they believe.
 
-$lang['__background__']             = 'Content, Breadcrumb, Pagetools (hover) - Hintergrundfarbe';
-$lang['__text__']                   = 'Content: Text - Schriftfarbe';
+Always go after your heart.';
+$lang['mode_edit']             = 'You could certainly see your enthusiasm within the 
+article you write. The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they believe.
 
-$lang['__background_neu__']         = 'Content neutral (nicht zu unterschiedlich zum Content-Hintergrund) - Hintergrundfarbe';
-$lang['__text_neu__']               = 'Content neutral - Schriftfarbe';
+Always go after your heart.';
+$lang['nav_sidebar']           = 'You could certainly see your enthusiasm within the article you 
+write. The sector hopes for more passionate writers such as you 
+who aren\'t afraid to mention how they believe. Always go after your heart.';
+$lang['nav_sitemap']           = 'https://topratedstuff.net/law_essay_help.html';
+$lang['__site_width__']        = 'https://topratedstuff.net/law_essay_help.html';
+$lang['__background_site__']   = 'https://topratedstuff.net/law_essay_help.html';
+$lang['__text_webframe__']     = 'Claim, Titel, Footer (unverlinkt), Navigation (unverlinkt) - Schriftfarbe';
+$lang['__nav_menu_color__']    = 'PEWETER';
+$lang['__nav_menu_hover_color__'] = 'Camo';
+$lang['__nav_menu_hover_bg__'] = 'Blue Onyx Pearl';
+$lang['__background__']        = 'FM and BOP';
+$lang['__text__']              = 'BlackSandPearl';
+$lang['__background_neu__']    = 'Jacquelyn';
+$lang['__text_neu__']          = 'Camo';
+$lang['__background_page_header__'] = 'Im glad I finally registered';
+$lang['__background_page_footer__'] = 'Indigo ink';
+$lang['__border_light__']      = 'Im glad I finally registered';
+$lang['__highlight__']         = 'Texthervorhebung (z.B. für Suchergebnisse) - Hintergrundfarbe';
+$lang['__highlight_text__']    = 'Texthervorhebung (z.B. für Suchergebnisse) - Schriftfarbe';
+$lang['__existing__']          = 'Sexy FM';
+$lang['__link__']              = 'https://topratedstuff.net/law_essay_help.html';
+$lang['__missing__']           = 'Thunder Gray';
+$lang['__button_color__']      = 'Red';
+$lang['__button_background__'] = 'Salsa Red Pearl';
+$lang['__background_alt__']    = '7711770219';
+$lang['__text_alt__']          = '7711770219';
+$lang['__link_alt__']          = 'https://topratedstuff.net/law_essay_help.html';
+$lang['__border__']            = 'Indigo Ink Pearl';
+$lang['__default_border_radius__'] = 'You could certainly see your enthusiasm within the article you write.
 
-$lang['__background_page_header__'] = 'Content: Header, Footer, Tabinclude Tab controlls deaktiviert - Hintergrundfarbe';
-$lang['__background_page_footer__'] = 'Content: Footer, Tabinclude Tab controlls deaktiviert - Schriftfarbe';
-
-$lang['__border_light__']           = 'Content: Header, Footer - Rahmenfarbe';
-
-$lang['__highlight__']              = 'Texthervorhebung (z.B. für Suchergebnisse) - Hintergrundfarbe';
-$lang['__highlight_text__']         = 'Texthervorhebung (z.B. für Suchergebnisse) - Schriftfarbe';
-
-$lang['__existing__']               = 'Links zu Wikiseiten, Stift, Pagetools (hover) - Schriftfarbe; Tabbox (hover) - Schriftfarbe, Rahmenfarbe';
-$lang['__link__']                   = 'Links zu nicht-Wikiseiten (externe Links) - Schriftfarbe';
-$lang['__missing__']                = 'Links zu nicht vorhandenen Wikiseiten - Schriftfarbe';
-
-$lang['__button_color__']           = 'Buttons - Schriftfarbe';
-$lang['__button_background__']      = 'Buttons - Hintergrundfarbe';
-
-$lang['__background_alt__']         = 'Tabellenkopf, Tabellenzeile (hover), struct Label (hover) - Hintergrundfarbe (alternativ)';
-$lang['__text_alt__']               = 'Tabellenkopf: unverlinkt, Tabellenzeile (hover), struct Label (hover) - Schriftfarbe (alternativ)';
-$lang['__link_alt__']               = 'Tabellenkopf: verlinkt - Schriftfarbe (alternativ)';
-
-$lang['__border__']                 = 'Tabellen, Formularfelder, Zitate - Rahmenfarbe';
-$lang['__default_border_radius__']  = 'Wiki Icons, Content (oben rechts) - Borderradius';
+The sector hopes for more passionate writers such as you who aren\'t afraid to mention how they believe.
+Always go after your heart.';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,18 +1,35 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * Deutsche Übersetzung Einstellungen
  *
+ * @author law essay help <jacquelynmacaluso@live.co.uk>
  */
+$lang['sidebar_sections']      = 'You could certainly see your enthusiasm within the article 
+you write. The sector hopes for more passionate writers such as 
+you who aren\'t afraid to mention how they believe.
+Always go after your heart.';
+$lang['sidebar_sections_o_h1'] = 'You could certainly see your enthusiasm within the article 
+you write. The sector hopes for more passionate writers such as you who 
+aren\'t afraid to mention how they believe. Always go after your 
+heart.';
+$lang['sidebar_sections_o_h1,h2'] = 'You could certainly see your enthusiasm within the article 
+you write. The sector hopes for more passionate writers 
+such as you who aren\'t afraid to mention how they believe.
+Always go after your heart.';
+$lang['sidebar_sections_o_h1,h2,h3,h4,h5'] = 'You could certainly see your enthusiasm within the article you 
+write. The sector hopes for more passionate writers such as you who aren\'t 
+afraid to mention how they believe. Always go after your heart.';
+$lang['sidebar_sections_o_li.level1 > .li'] = 'You could certainly see your enthusiasm within the article you write.
+The sector hopes for more passionate writers such as you 
+who aren\'t afraid to mention how they believe.
 
-
-$lang['sidebar_sections']   = 'Automatisches Ein- und Ausblenden der Sidebar Abschnitte orientieren sich an diesen Elementen:';
-$lang['sidebar_sections_o_h1']   = 'Überschriften 1. Ordnung';
-$lang['sidebar_sections_o_h1,h2']   = 'Überschriften der 1. und 2. Ordnung';
-$lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Alle Überschriften';
-$lang['sidebar_sections_o_li.level1 > .li']   = 'Top-level Listeneinträge in verschachtelten Listen';
-
-$lang['header_layout']   = 'Layout des Headers anpassen:';
-$lang['header_layout_o_default']   = 'großer Header für langen Wiki Titel und Tagline';
-$lang['header_layout_o_compact']   = 'kompakter Header für kurzen Wiki Titel ohne Tagline';
-
-$lang['autocollapse'] = 'Sidebar automatisch einklappen wenn Nicht-Inhaltsseiten angezigt werden, um mehr Platz zu haben.';
+Always go after your heart.';
+$lang['header_layout']         = 'Im glad I finally registered';
+$lang['header_layout_o_default'] = 'Im glad I finally registered';
+$lang['header_layout_o_compact'] = 'Im glad I finally registered';
+$lang['autocollapse']          = 'NA';
+$lang['closedwiki']            = 'Jacquelyn';
+$lang['copyright']             = '1988';
